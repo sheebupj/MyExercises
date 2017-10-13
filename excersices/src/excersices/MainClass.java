a4cd80219e5f9d4faf2b95c7c50677818e7b8791
sheebu PJ <sheebupj@gmail.com>
2017-10-13 10:32:27 +0530

    swaping 1234 become 3412

--- excersices/src/excersices/MainClass.java
+++ excersices/src/excersices/MainClass.java
@@ -4,12 +4,16 @@ public class MainClass
 {
     public static void main(String[] args)
     {
-        try
-        {
-            Class.forName("oracle.jdbc.driver.OracleDriver");
-        }catch (ClassNotFoundException e)
-        {
-            e.printStackTrace();
-        }
+//        try
+//        {
+//            Class.forName("oracle.jdbc.driver.OracleDriver");
+//        }catch (ClassNotFoundException e)
+//        {
+//            e.printStackTrace();
+//        }
+    	int[] a= new int[2];
+        a[0]=1;
+       // a=a+'c';
+        System.out.println(a[0]);
     }
 }
