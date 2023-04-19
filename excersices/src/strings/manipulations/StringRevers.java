381e8f666856966f6d6bd1f589040fc862dfb095
sheebu PJ <sheebupj@gmail.com>
2023-04-19 11:11:34 +0530

    Counting of special character in a string added

--- excersices/src/strings/manipulations/StringRevers.java
+++ excersices/src/strings/manipulations/StringRevers.java
@@ -1,7 +1,14 @@
+package strings.manipulations;
+
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class StringRevers {
 	public static void main(String[] args) {
+		
+		/*
+		 * sentence reversal letter wise
+		 */
 		String txt= "My name is Sheebu";
 		int len= txt.length();
 		String reversed="";
@@ -9,13 +16,20 @@ public class StringRevers {
 			reversed=reversed+txt.charAt(i);
 		}
 		System.out.println(reversed);
-		//word wise reverse
+		/*
+		 * sentence reversal word wise
+		 */
 		String[] txtAr= txt.split(" ");
 		 reversed="";
 		for(int i=txtAr.length-1;i>=0;i--) {
 			reversed= reversed+ txtAr[i]+" ";
 		}
 		System.out.println(reversed);
+		/*
+		 * count special characters in a string
+		 */
+		String str="abcdy&jdkg%dfjd/sjjdfd*uenbgd@pyebdfd (";
+		System.out.println(Arrays.stream(str.split("")).map(s->s.charAt(0)).filter(c-> !(Character.isAlphabetic(c)|| Character.isDigit(c))).count());
 		
 	}
 
